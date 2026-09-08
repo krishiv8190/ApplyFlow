3a92e28561cea330c4809a6f278e058d9f1b93fe
Krishiv <avin.pandita32@gmail.com>
2026-09-08 16:57:12 +0530

    chore(server): scaffold express service

---
+++ server/eslint.config.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.ts'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  eslintConfigPrettier,
+]);
